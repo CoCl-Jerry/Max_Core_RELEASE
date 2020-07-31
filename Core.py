350c57fce88e9fa5b2bfc2e9bb38deda68166787
Jerry <jerry@cosecloud.com>
2020-07-31 12:31:13 -0500

    Baseline Version
    Baseline Version uploaded

--- Core.py
+++ Core.py
@@ -13,48 +13,35 @@ while True:
     connection, client_address = sock.accept()
 
     while True:
-        recieved = connection.recv(1024).decode("utf-8")
+        try:
+            recieved = connection.recv(1024).decode("utf-8")
+            print(recieved)
 
-        CMD = recieved.split('~', 5)
+            CMD = recieved.split('~', 9)
 
-        if(CMD[0]=='A'):
-            with PiCamera() as camera:
-                camera.resolution = (int(CMD[1]),int(CMD[2]))
-                camera._set_rotation(90*int(CMD[3]))
-                sleep(2)
-                if(int(CMD[4])==1):
-                    camera.capture("out.jpg")
+            if(CMD[0] == 'A'):
+                with PiCamera() as camera:
+                    camera.resolution = (int(CMD[1]), int(CMD[2]))
+                    camera._set_rotation(90 * int(CMD[3]))
+                    camera.zoom = (
+                        int(CMD[4]) / 100, int(CMD[5]) / 100, int(CMD[6]) / 100, int(CMD[7]) / 100)
+                    sleep(2)
+                    if(int(CMD[8]) == 1):
+                        camera.capture("out.jpg")
+                    else:
+                        camera.capture("out.png")
+
+                if(int(CMD[8]) == 1):
+                    f = open("out.jpg", "rb")
                 else:
-                    camera.capture("out.png")
-            
-            if(int(CMD[4])==1):
-                f = open ("out.jpg", "rb")
-            else:
-                f = open ("out.png", "rb")
-            l = f.read(512)
-            while (l):
-                connection.send(l)
+                    f = open("out.png", "rb")
                 l = f.read(512)
-            f.close()
-            break
-
-        elif(CMD[0]=='B'):
-            with PiCamera() as camera:
-                camera.resolution = (350,350)
-                camera.zoom = (0.25,0.25,0.5,0.5)
-                sleep(4)
-                camera.capture("out.jpg")
-            f = open ("out.jpg", "rb")
-                
-            l = f.read(512)
-            while (l):
-                connection.send(l)
-                l = f.read(512)
-            f.close()
-            break
+                while (l):
+                    connection.send(l)
+                    l = f.read(512)
+                f.close()
+                break
+        except Exception as e:
+            print(e)
 
     connection.close()
-    
-
-
-
